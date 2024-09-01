dc3fe01a3b9984a65039baf0d22945a295e69209
Carvier <60159324+carvierdotdev@users.noreply.github.com>
2024-09-01 17:08:11 -0400

    feat: update site metadata and image permissions
    •  Updated `ava.jpg` and `small-ava.jpg` file permissions
    
    •  Changed site title, tagline, and description in `consts.ts`
    
    commit-id:b95f9730

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'Carvier.dev';
+export const SITE_TAGLINE = 'Empowering Developers, Enhancing Productivity, Embracing Wellness'; // site tagline
+export const SITE_DESCRIPTION =
+	'Empowering individuals with curate knowledge from high-performance individuals and best-in-class learning-sites. Dive into digital skills, productivity hacks, fitness tips, and spiritual growth. Join the journey at carvier.dev for free fundamental knowledge and more goodies.'; // site meta description for SEO
